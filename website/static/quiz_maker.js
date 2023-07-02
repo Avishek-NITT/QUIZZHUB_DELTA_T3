a0d03902beb5310f452c6bca28188e579cd6e93a
Falcon <avishekmajumder2004@gmail.com>
2023-07-02 17:05:34 +0530

    Fixed some bugs in quiz_maker

--- website/static/quiz_maker.js
+++ website/static/quiz_maker.js
@@ -5,21 +5,14 @@ let ques_count = 1
 let htmlmarkup = ""
 
 form_ele.addEventListener('submit', event =>{
-
-
-
     event.preventDefault();
     const formData = new FormData(form_ele)
     const data = Object.fromEntries(formData)
+    if(check_quiz(data,ques_count)){
 
-
-    if(check_quiz(data)){
     }else{
         return
     }
-   
-
-
     fetch('http://127.0.0.1:5000/quizmaker', {
         method :'POST',
         headers:{
@@ -28,7 +21,6 @@ form_ele.addEventListener('submit', event =>{
         body: JSON.stringify(data)
     })
 })
-
 
 function add_ques(){
     console.log("Hello")
@@ -43,20 +35,20 @@ function add_ques(){
     htmlmarkup += `
     <div class="question" id="question${ques_count}">
             <h3> Question ${ques_count} </h3>
-            <input type="text" class="text2" id="question${ques_count}" name="question${ques_count}"/>
+            <input type="text" class="text text2" id="question${ques_count}" name="question${ques_count}"/>
             <br>
             <label class="text2"> Enter Correct option </label> 
             <br>
-            <input class="text2" type="text" id="q${ques_count}_c1" name="q${ques_count}_c1"/>
+            <input class="text text2" type="text" id="q${ques_count}_c1" name="q${ques_count}_c1"/>
             <br>
             <label class="text2"> Enter other options </label><br>
-            <input class="text2" type="text" id="q${ques_count}_c2" name="q${ques_count}_c2"/>
+            <input class="text text2" type="text" id="q${ques_count}_c2" name="q${ques_count}_c2"/>
             <br>
             <br>
-            <input class="text2" type="text" id="q${ques_count}_c3" name="q${ques_count}_c3"/>
+            <input class="text text2" type="text" id="q${ques_count}_c3" name="q${ques_count}_c3"/>
             <br>
             <br>
-            <input class="text2" type="text" id="q${ques_count}_c4" name="q${ques_count}_c4"/>
+            <input class="text text2" type="text" id="q${ques_count}_c4" name="q${ques_count}_c4"/>
         </div>
         <button id="add_que_btn" onclick="add_ques()">+</button>
     `
@@ -65,7 +57,7 @@ function add_ques(){
 
 
 
-function check_quiz(data){
+function check_quiz(data, total_ques){
     if (!data.quiz_name){
         alert("Quiz must have a name")
         return 0
@@ -74,19 +66,20 @@ function check_quiz(data){
         alert("Quiz must have a description")
         return 0
     }
-    let ques_counter = 1
-    for (; ques_counter <6 ; ques_counter++){
-        if(`!data.question${ques_counter}`){
-            alert(`Question ${ques_counter} is not provided`)
-            return 0
-        }
+    
+    for (let ques_counter = 1; ques_counter <= total_ques ; ques_counter++){
+        if (!data[`question${ques_counter}`]) {
+            alert(`Question ${ques_counter} is not provided`);
+            return 0;
+          }
         for(let i = 1; i < 5 ; i++){
-            if(`!data.q${ques_counter}_c${i}`){
+            if(!data[`q${ques_counter}_c${i}`]){
                 alert("Fill in all the options")
                 return 0 
             }
         }
     }
+    return 1
 }
 
 
